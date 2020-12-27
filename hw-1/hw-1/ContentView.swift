ab732a9d8b83a4d1a5a07f6fc8085e8484b0fbeb
Ahmed the Green <67065998+Ahmed-Bader@users.noreply.github.com>
2020-12-27 13:09:07 +0300

    partially solved hw1

--- hw-1/hw-1/ContentView.swift
+++ hw-1/hw-1/ContentView.swift
@@ -9,8 +9,131 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack
+        {
+            Color.black
+            
+            VStack
+            {
+                Spacer()
+                Spacer()
+                Spacer()
+                HStack
+                {
+                    Spacer()
+                    Text("555")
+                        .font(.system(size: 70))
+                        .fontWeight(.bold)
+                        .padding()
+//                        .frame(width: 200.0, height: 200.0)
+                }
+                
+                HStack(spacing: 50)
+                {
+                    VStack{
+                        Text("C")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("7")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("4")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("1")
+                            .font(.title)
+                            .padding()
+                            
+                            
+                    }
+                    
+                    
+                    VStack{
+                        Text("±")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("8")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("5")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("2")
+                            .font(.title)
+                            .padding()
+                        
+                    }
+                    
+                    VStack{
+                        Text("%")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("9")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("6")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("3")
+                            .font(.title)
+                            .padding()
+                        
+                        //                        Text(".")
+                        //                            .font(.title)
+                        //                            .padding()
+                    }
+                    
+                    VStack{
+                        Text("÷")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("x")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("-")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("+")
+                            .font(.title)
+                            .padding()
+                    }
+                }
+
+                HStack
+                {
+                    Text("0")
+                        .font(.title)
+                        .padding(.horizontal, 80)
+                        .padding(.vertical)
+                        
+                    
+                    
+                    Text(".")
+                        .font(.title)
+                        .padding(.horizontal, 30.0)
+                    
+                    Text("-")
+                        .font(.title)
+                        .padding(.horizontal, 40.0)
+                }
+            Spacer()
+            }
+            
+        }
+        .foregroundColor(.white)
+        .ignoresSafeArea()
     }
 }
 
