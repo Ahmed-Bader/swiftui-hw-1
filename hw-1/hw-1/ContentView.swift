1a7070263069c2b67e8d1f2e841de1175428443d
Ahmed the Green <67065998+Ahmed-Bader@users.noreply.github.com>
2020-12-27 11:19:03 +0300

    creating hw
    ran into same issues with xcode not running preview because file being in repository for some reason.

---
+++ hw-1/hw-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw-1
+//
+//  Created by Eng.Ahmed Bader on 27/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw-1/hw-1/hw_1App.swift
@@ -0,0 +1,17 @@
+//
+//  hw_1App.swift
+//  hw-1
+//
+//  Created by Eng.Ahmed Bader on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct hw_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
